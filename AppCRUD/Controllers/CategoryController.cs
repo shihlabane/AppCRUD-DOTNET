5d0544c40b69a5746fc560881a1672884c2cdf4a
Ishmael <79452939+shihlabane@users.noreply.github.com>
2023-03-07 05:17:02 +0200

    first commint

---
+++ AppCRUD/Controllers/CategoryController.cs
@@ -0,0 +1,108 @@
+﻿using AppCRUD.Data;
+using AppCRUD.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCRUD.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        //Get -Display
+        public IActionResult Index()
+        {
+
+            IEnumerable<Category> objList = _db.category;
+
+            return View(objList);
+        }
+        //Get -Create
+        public IActionResult Create()
+        {
+
+            return View();
+        }
+        //Post-Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+        //Get - Edit
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //Post-Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+
+            }
+            return View(obj);
+        }
+
+       
+        //Get - Delete
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        //Post-Delete
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.category.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+
+            return View(obj);
+        }
+    }
+}
---
+++ AppCRUD/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AppCRUD.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppCRUD.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Category> category { get; set; }
+    }
+}
